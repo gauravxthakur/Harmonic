b7486b13d6f641b93fa3beda9cc9f160c2cf829c
gxt <gauravkthakur2002@gmail.com>
2025-08-01 05:41:57 +0530

    set up to check on 1st render for a session with getSession()

--- AuthContext.jsx
+++ AuthContext.jsx
@@ -1,13 +1,53 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
+import supabase from "./src/supabase-client";
 
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({children}) =>{
-    //Auth functions (signin, signup, logout)
-
-
-    // //Session state (user info, sign-in status)
+    //Session state (user info, sign-in status)
     const [session, setSession] = useState(undefined);
+
+    useEffect(() => {
+        //1) Check on 1st render for a session (getSession())
+
+        async function getInitialSession(){
+            try{
+                const {data, error} = await supabase.auth.getSession();
+                if (error){
+                    throw error;
+                }
+                // success
+                console.log(data.session);
+                setSession(data.session);
+            }catch(error){
+                console.error("Error getting session:", error.message);
+
+            }
+        }
+
+        getInitialSession();
+        /**
+      Challenge:
+      * 1) Inside the useEffect, write an asynchronous 'getInitialSession' function 
+           which executes the 'auth.getSession()' method from the Supabase client 
+           library
+      * 2) Destructure the response into 'data' and 'error' variables
+      * 3) Using the try/catch syntax, handle both Supabase-specific errors and 
+           unexpected errors
+      * 4) If successful, log the value of the 'session' property of the 'data' 
+           object to the console and then use it to update the 'session' state 
+      * 5) Call the new function below the function definition
+      * 6) Check your browser's dev tools for 'null' being logged
+           Hint: follow the same error handling pattern as the 'fetchMetrics' function
+      **/
+      
+        //2) Listen for changes in auth state
+      
+        }, []);
+
+//Auth functions (signin, signup, logout)
+
+
 
     return (
         <AuthContext.Provider value={{session}}>
