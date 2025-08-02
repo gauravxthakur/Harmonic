ae3a317527eb86c7bd8d4ab4350c6be718cce8b8
gxt <gauravkthakur2002@gmail.com>
2025-08-02 08:15:42 +0530

    set up Root Redirect component

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,13 @@
-import { createBrowserRouter } from 'react-router-dom';
-import Signin from './components/signin';
-import Dashboard from './routes/Dashboard';
-import Header from './components/header';
-import Signup from './components/signup';
+import { createBrowserRouter } from "react-router-dom";
+import Signin from "./components/signin";
+import Dashboard from "./routes/Dashboard";
+import Header from "./components/header";
+import Signup from "./components/signup";
+import RootRedirect from "./routes/RootRedirect";
 
 export const router = createBrowserRouter([
-    {path:"/", element: <Signin />},
-    {path:"/dashboard", element: (<><Header /> <Dashboard /></>)},
-    {path:"/signup", element: <Signup />}
+  {path: "/", element: <RootRedirect />},
+  {path: "/signin", element: <Signin />},
+  {path: "/signup", element: <Signup />},
+  {path: "/dashboard", element: (<> <Header /> <Dashboard /> </>)}
 ]);
